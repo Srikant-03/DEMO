324cfa4d8627bf08b4a14ce596f44f29f2da2746
bhavy tandon <bhavya.t145@gmail.com>
2025-08-07 15:10:14 +0530

    multiplication

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,11 @@
 
+package DEMO;
+public class Calculator {
+    public static void main(String[] args) {
+        
+    }
+
+    public  int multiply(int x, int y) {
+        return x * y;
+    }
+}
